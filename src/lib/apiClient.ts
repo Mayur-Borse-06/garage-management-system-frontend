60b38d430c2d3ee0af2ccb0ddd6a68afa9b31064
Mayur-Borse-06 <mayurborse9119@gmail.com>
2026-09-09 00:57:07 +0530

    added favicon

--- src/lib/apiClient.ts
+++ src/lib/apiClient.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const apiClient = axios.create({
-  baseURL: import.meta.env.VITE_API_URL,
+  baseURL: import.meta.env.VITE_API_BASE_URL,
   timeout: 10000, // Timeout limit (10 seconds)
   headers: {
     'Content-Type': 'application/json',
